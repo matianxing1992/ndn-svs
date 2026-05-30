aa746fa10fae80768568214d160fc2df133fc58d
matianxing1992 <matianxing1992@gmail.com>
2026-05-29 22:52:58 -0500

    svs: implement standards-compliant V3 sync protocol

--- ndn-svs/svsync-base.hpp
+++ ndn-svs/svsync-base.hpp
@@ -53,7 +53,8 @@ public:
              ndn::Face& face,
              const UpdateCallback& updateCallback,
              const SecurityOptions& securityOptions = SecurityOptions::DEFAULT,
-             std::shared_ptr<DataStore> dataStore = DEFAULT_DATASTORE);
+             std::shared_ptr<DataStore> dataStore = DEFAULT_DATASTORE,
+             const SyncProtocolOptions& protocolOptions = {});
 
   virtual ~SVSyncBase() = default;
 
@@ -170,6 +171,12 @@ public:
                  const DataValidatedCallback& onValidated,
                  int nRetries = 0);
 
+  void fetchData(const NodeID& nid,
+                 const BootstrapTime& bootstrapTime,
+                 const SeqNo& seq,
+                 const DataValidatedCallback& onValidated,
+                 int nRetries = 0);
+
   /**
    * @brief Retrive a data packet with a particular seqNo from a session
    *
@@ -189,6 +196,14 @@ public:
                  const TimeoutCallback& onTimeout,
                  int nRetries = 0);
 
+  void fetchData(const NodeID& nid,
+                 const BootstrapTime& bootstrapTime,
+                 const SeqNo& seq,
+                 const DataValidatedCallback& onValidated,
+                 const DataValidationErrorCallback& onValidationFailed,
+                 const TimeoutCallback& onTimeout,
+                 int nRetries = 0);
+
   /** @brief Get the underlying data store */
   DataStore& getDataStore()
   {
@@ -201,6 +216,21 @@ public:
     return m_core;
   }
 
+  const SVSyncCore& getCore() const
+  {
+    return m_core;
+  }
+
+  Name getDataName(const NodeID& nid, const BootstrapTime& bootstrapTime, const SeqNo& seqNo)
+  {
+    return makeDataName(nid, bootstrapTime, seqNo);
+  }
+
+  Name getDataName(const NodeID& nid, const SeqNo& seqNo)
+  {
+    return makeDataName(nid, m_core.getBootstrapTime(), seqNo);
+  }
+
 protected:
   /**
    * @brief Return data name for a given packet
@@ -210,7 +240,8 @@ protected:
    * data prefix for proper functionality, or the application must
    * independently produce data under the prefix.
    */
-  virtual Name getDataName(const NodeID& nid, const SeqNo& seqNo) = 0;
+  virtual Name makeDataName(const NodeID& nid, const BootstrapTime& bootstrapTime,
+                            const SeqNo& seqNo) = 0;
 
 public:
   static inline const NodeID EMPTY_NODE_ID;
